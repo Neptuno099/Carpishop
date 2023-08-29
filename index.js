cdf7e9e3986a238b58157cf24fae272af7259dc9
Neptuno099~ <jpdavila099@gmail.com>
2023-08-29 13:38:23 -0500

    Añadiendo funcionalidad al carrito

--- index.js
+++ index.js
@@ -121,6 +121,7 @@ const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let productoAgregar = document.querySelector(".producto-agregar");
+const numerito = document.querySelector("#numerito");
 
 
 function cargarProductos(productosElegidos){
@@ -179,19 +180,37 @@ function actualizarBotonesAgregar(){
 }
 
 
+let productosEnCarrito;
 
-const productosEnCarrito = [];
+let productosEnCarritoLS = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+if (productosEnCarritoLS){
+    productosEnCarrito = productosEnCarritoLS;
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+};
+
 
 function agregarAlCarrito(e){
     const idBoton = e.currentTarget.id;
-    const productoAgregado = productos.find(producto => productos.id === idBoton);
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
 
 
-    if(productosEnCarrito.some(producto => productos.id === idBoton)){
-
+    if(productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
     } else{
         productoAgregado.cantidad = 1;
         productosEnCarrito.push(productoAgregado);
     }
 
+    actualizarNumerito();
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+}
+
+function actualizarNumerito(){
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerHTML = nuevoNumerito;
 }
